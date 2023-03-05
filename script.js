8af0fbffa6aee1dc89ec9d1c7bb0dec482adbecd
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-06 00:26:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const itemNameInput = document.getElementById('item-name-input');
+const itemPriceInput = document.getElementById('item-price-input');
+const addButton = document.getElementById('add-button');
+const itemList = document.getElementById('item-list');
+const grandTotalElement = document.querySelector('[data-ns-test="grandTotal"]');
+
+let grandTotal = 0;
+
+addButton.addEventListener('
+
